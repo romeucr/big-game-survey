c8741b96de0ba56d11c994faf94a750f7822064e
Romeu Castro Rodrigues <romeu.castro@gmail.com>
2020-09-14 20:16:57 +0200

    Project Created

---
+++ backend/src/test/java/com/rcrdev/dspesquisa/DspesquisaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rcrdev.dspesquisa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DspesquisaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
