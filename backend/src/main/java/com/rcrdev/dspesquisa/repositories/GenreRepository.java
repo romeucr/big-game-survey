5c261a331205bd07c91417d001414fd8cd83c417
Romeu Castro Rodrigues <romeu.castro@gmail.com>
2020-09-15 00:01:28 +0200

    GameDTO, GameService

--- backend/src/main/java/com/rcrdev/dspesquisa/repositories/GenreRepository.java
+++ backend/src/main/java/com/rcrdev/dspesquisa/repositories/GenreRepository.java
@@ -1,9 +1,11 @@
 package com.rcrdev.dspesquisa.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.rcrdev.dspesquisa.entities.Genre;
 
+@Repository
 public interface GenreRepository extends JpaRepository<Genre, Long> {
 
 }
