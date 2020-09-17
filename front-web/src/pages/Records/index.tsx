4ba9d17f6ae42a48f124c26dbbd72c43c621ffbd
Romeu Castro Rodrigues <romeu.castro@gmail.com>
2020-09-17 23:16:10 +0200

    Adicionando a pagina de graficos

--- front-web/src/pages/Records/index.tsx
+++ front-web/src/pages/Records/index.tsx
@@ -4,32 +4,26 @@ import './styles.css'
 import { RecordsResponse } from './types';
 import { formatDate } from './helpers';
 import Pagination from './Pagination';
-import { Link } from 'react-router-dom'
+import Filters from '../../components/Filters';
 
 const BASE_URL = "http://localhost:8080"
+
 const Records = () => {
     const [recordsResponse, setRecordsResponse] = useState<RecordsResponse>();
-    const  [activePage, setActivePage] = useState(0);
+    const [activePage, setActivePage] = useState(0);
 
     useEffect(() => {
         axios.get(`${BASE_URL}/records?linesPerPage=12&page=${activePage}`)
             .then(response => setRecordsResponse(response.data));
     }, [activePage]);
 
-const handlePageChange = (index: number) => {
-    setActivePage(index)
-}
+    const handlePageChange = (index: number) => {
+        setActivePage(index);
+    }
 
     return (
         <div className="page-container">
-            <div className="filters-container records-actions">
-            <Link to='/charts'>
-                <button className="action-filters">
-                    VER GRAFICOS
-                </button>
-            </Link>
-
-            </div>
+            <Filters link="/charts" linkText="VER GRAFICOS" />
             <table className="records-table" cellPadding="0" cellSpacing="0">
                 <thead>
                     <tr>
@@ -60,7 +54,7 @@ const handlePageChange = (index: number) => {
             totalPages={recordsResponse?.totalPages}
             />
 
-        </div>
+        </div >
     );
 }
 
